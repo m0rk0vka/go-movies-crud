84b42ab099d4622ea3f5b82364d068fb9834911e
t.vladimir <vovatatosyan2000@mail.ru>
2023-07-29 22:19:30 +0300

    Added routes

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/gorilla/mux"
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -13,3 +15,13 @@ type Director struct {
 }
 
 var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteFunc).Methods("DELETE")
+}

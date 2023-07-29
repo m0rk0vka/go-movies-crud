43d2abb19a2bfb3a854f9b1749ca7d7680de591b
t.vladimir <vovatatosyan2000@mail.ru>
2023-07-29 23:10:31 +0300

    Changed route delete function name

--- main.go
+++ main.go
@@ -114,7 +114,7 @@ func main() {
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
-	r.HandleFunc("/movies/{id}", deleteFunc).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
